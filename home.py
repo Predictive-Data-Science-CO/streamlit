d6f5a9313101567490b1e74b4346f0fc4505a8fe
lzou <lizou310@gmail.com>
2024-10-05 07:55:14 -0500

    remove login

--- home.py
+++ home.py
@@ -33,26 +33,27 @@ parametric_test = st.Page("statistics/parametric_test.py", title="Parametric Tes
 simple_regression = st.Page("statistics/simple_regression.py", title="Simple Regression", icon=":material/info:")
 #Machine Learning
 linear_regression = st.Page("ml/linear_regression.py", title="Linear Regression", icon=":material/info:")
+model_selection = st.Page("ml/model_selection.py", title="Model Selection", icon=":material/info:")
 outlier_detection = st.Page("ml/outlier_detection.py", title="Outlier Detection", icon=":material/info:", default=(role == "ML"))
-ml_1 = st.Page("ml/ml_1.py", title="ml 1", icon=":material/info:")
-ml_2 = st.Page("ml/ml_2.py", title="ml 2", icon=":material/info:")
 
 account_pages = [logout_page, settings]
 statistics_pages = [stats_types, logistic_regression, multiple_regression, non_parametric_test, parametric_test, simple_regression]
-ml_pages = [linear_regression, outlier_detection, ml_1, ml_2]
+ml_pages = [linear_regression, model_selection, outlier_detection]
 
 # st.title("Select Project")
-st.logo("images/horizontal_blue.png", icon_image="images/icon_blue.png")
+# st.logo("images/horizontal_blue.png", icon_image="images/icon_blue.png")
 
 page_dict = {}
-if st.session_state.role in ["Statistics"]:
-    page_dict["statistics"] = statistics_pages
-if st.session_state.role == "ML":
-    page_dict["ml"] = ml_pages
+#if st.session_state.role in ["Statistics"]:
+page_dict["statistics"] = statistics_pages
+#if st.session_state.role == "ML":
+page_dict["ml"] = ml_pages
 
-if len(page_dict) > 0:
-    pg = st.navigation({"Account": account_pages} | page_dict)
-else:
-    pg = st.navigation([st.Page(login)])
+pg = st.navigation(page_dict)
+
+# if len(page_dict) > 0:
+#     pg = st.navigation({"Account": account_pages} | page_dict)
+# else:
+#     pg = st.navigation([st.Page(login)])
 
 pg.run()
