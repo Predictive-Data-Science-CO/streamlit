7732ca90ebba39f978dec521f074a206ac5ec03c
PDS Code <li.zou@pdtds.com>
2024-10-12 11:58:16 -0500

    initial

--- statistics/logistic_regression.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.markdown(""" 
-            ## Logistic Regression
-            - sample question
-            1. What is the effect of drug dosage on the survival of a test subject?
-            """)
